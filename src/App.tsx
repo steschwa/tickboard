62cf1a5a175368c4e128bf1fc822786ca624f4bf
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-03-17 22:04:58 +0100

    feat: WIP content

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { Haupthalle } from "./components/hallen/bloc-huette/Haupthalle"
 import { Header } from "./components/header/Header"
 import { Toolbar } from "./components/toolbar/Toolbar"
 
 export function App() {
     return (
-        <>
+        <div className="h-dvh flex flex-col">
             <Header />
             <Toolbar />
-        </>
+
+            <div className="flex-1 p-2 flex justify-center items-center">
+                <Haupthalle />
+            </div>
+        </div>
     )
 }
