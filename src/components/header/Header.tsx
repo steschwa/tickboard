3f8bca66ab6fbc2a8bfa0f68ec6913951d33ab7c
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-03-18 19:59:39 +0100

    feat: initial halle

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,15 @@
+import { useAtomValue } from "jotai"
+import type { Halle } from "lib/types"
 import { MenuIcon } from "lucide-react"
+import { halleAtom } from "stores/halle"
 
 export function Header() {
+    const halle = useAtomValue(halleAtom)
+
     return (
         <header className="border-b border-gray-100 flex items-center justify-between px-4 py-2">
             <h1 className="text-gray-900 font-semibold text-base">
-                Bloc-Hütte Haupthalle
+                {formatHalle(halle)}
             </h1>
 
             <button type="button" className="text-gray-500 focus:outline-none">
@@ -12,4 +17,15 @@ export function Header() {
             </button>
         </header>
     )
+}
+
+function formatHalle(halle: Halle): string {
+    switch (halle) {
+        case "BLOC_HUETTE_HAUPTHALLE":
+            return "Bloc-Hütte Haupthalle"
+        case "BLOC_HUETTE_AUSSENBEREICH":
+            return "Bloc-Hütte Aussenbereich"
+        case "BLOC_HUETTE_NEUEHALLE":
+            return "Bloc-Hütte Neue Halle"
+    }
 }
--- src/stores/halle.ts
+++ src/stores/halle.ts
@@ -1,4 +1,4 @@
 import { atom } from "jotai"
-import { HALLE, type Halle } from "lib/types"
+import type { Halle } from "lib/types"
 
-export const halleAtom = atom<Halle>(HALLE.BLOC_HUETTE_HAUPTHALLE)
+export const halleAtom = atom<Halle>("BLOC_HUETTE_HAUPTHALLE")
