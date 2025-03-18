68b2b87aa66b2b0531410a6a472e528859aa0fd8
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-03-18 21:09:05 +0100

    feat: drawer transitions

--- src/components/drawer/Drawer.tsx
+++ src/components/drawer/Drawer.tsx
@@ -1,4 +1,5 @@
 import { Dialog } from "@base-ui-components/react/dialog"
+import clsx from "clsx"
 import { XIcon } from "lucide-react"
 
 type DrawerProps = {
@@ -15,8 +16,17 @@ type ContentProps = {
 function Content(props: ContentProps) {
     return (
         <Dialog.Portal>
-            <Dialog.Backdrop className="bg-gray-900/20 shadow fixed inset-0" />
-            <Dialog.Popup className="fixed top-2 bottom-2 right-0 bg-white rounded-tl-2xl rounded-bl-2xl max-w-4/5 w-full p-4">
+            <Dialog.Backdrop
+                className={clsx(
+                    "bg-gray-900 shadow fixed inset-0",
+                    "transition-opacity opacity-20 data-[starting-style]:opacity-0 data-[ending-style]:opacity-0",
+                )}
+            />
+            <Dialog.Popup
+                className={clsx(
+                    "fixed top-2 bottom-2 right-0 bg-white rounded-tl-2xl rounded-bl-2xl max-w-4/5 w-full p-4",
+                    "transition-all opacity-100 translate-x-0 data-[starting-style]:opacity-0 data-[starting-style]:translate-x-1/2 data-[ending-style]:opacity-0 data-[ending-style]:translate-x-1/2",
+                )}>
                 <div className="flex items-center justify-between mb-6">
                     <Dialog.Title className="text-base font-semibold text-gray-900">
                         {props.title}
