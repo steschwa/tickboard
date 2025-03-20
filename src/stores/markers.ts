8d5236956e5465ee9014d0055dfd672dadba663e
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-03-20 21:22:57 +0100

    refactor: rename

--- src/stores/markers.ts
+++ src/stores/markers.ts
@@ -1,4 +1,4 @@
 import type { Marker } from "@/lib/marker"
 import { atomWithStorage } from "jotai/utils"
 
-export const markersStore = atomWithStorage<Marker[]>("markers", [])
+export const markersAtom = atomWithStorage<Marker[]>("markers", [])
