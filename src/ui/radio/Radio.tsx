9414651407598a2ab3de4637e6fa7e93b5521bde
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-03-25 17:39:40 +0100

    refactor: radio-group in same file as radio

--- src/ui/radio/Radio.tsx
+++ src/ui/radio/Radio.tsx
@@ -1,4 +1,44 @@
 import { Radio as RadioPrimitive } from "@base-ui-components/react/radio"
+import { RadioGroup as RadioGroupPrimitive } from "@base-ui-components/react/radio-group"
+import { useId } from "react"
+
+type RadioGroupProps = {
+    title?: React.ReactNode
+    value?: string
+    defaultValue?: string
+    onValueChange?: (value: string) => void
+
+    children: React.ReactNode
+}
+export function RadioGroup(props: RadioGroupProps) {
+    const labeledById = useId()
+
+    const handleValueChange = (value: unknown) => {
+        if (typeof value !== "string") {
+            return
+        }
+
+        props.onValueChange?.(value)
+    }
+
+    return (
+        <RadioGroupPrimitive
+            value={props.value}
+            defaultValue={props.defaultValue}
+            onValueChange={handleValueChange}
+            aria-labelledby={props.title ? labeledById : undefined}>
+            {props.title && (
+                <h4
+                    id={labeledById}
+                    className="mb-3 text-sm font-medium text-gray-900">
+                    {props.title}
+                </h4>
+            )}
+
+            <div className="flex flex-col gap-y-2">{props.children}</div>
+        </RadioGroupPrimitive>
+    )
+}
 
 type RadioProps = {
     value: string
