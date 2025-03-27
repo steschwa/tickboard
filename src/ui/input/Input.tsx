4c66f01e12e50389675994fd65e095a7c6e4d987
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-03-27 22:32:34 +0100

    feat: add input focus ring

--- src/ui/input/Input.tsx
+++ src/ui/input/Input.tsx
@@ -1,10 +1,16 @@
+import clsx from "clsx"
+
 type InputProps = React.ComponentPropsWithoutRef<"input">
 
 export function Input(props: InputProps) {
     return (
         <input
             {...props}
-            className="h-10 px-3 rounded-lg border border-gray-200 text-gray-900 bg-white text-sm focus:outline-none placeholder:text-gray-400"
+            className={clsx(
+                "h-10 px-3 rounded-lg border border-gray-200 text-gray-900 bg-white text-sm",
+                "placeholder:text-gray-400",
+                "focus:outline-3 outline-gray-100",
+            )}
         />
     )
 }
