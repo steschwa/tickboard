4d1e97b58dc54c6121201b4144ecacd07897221f
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-04-03 22:26:02 +0200

    feat: bigger spacing

--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -12,7 +12,7 @@ export function Toolbar(props: ToolbarProps) {
         <div className="border-b border-gray-100 flex items-center justify-between px-4 py-2">
             <GymLevelSelect />
 
-            <div className="flex justify-end gap-x-2 items-center">
+            <div className="flex justify-end gap-x-4 items-center">
                 <IconButton onClick={props.onShare}>
                     <ShareIcon />
                 </IconButton>
