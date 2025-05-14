21e5567ff11f1ea1c4b12d354e43c564103bb0c6
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-05-14 21:23:46 +0200

    feat: improve bloc huette haupthalle

--- src/components/gyms/bloc-huette/BlocHuetteHaupthalle.tsx
+++ src/components/gyms/bloc-huette/BlocHuetteHaupthalle.tsx
@@ -9,126 +9,144 @@ export function BlocHuetteHaupthalle(
             <title>Bloc-Hütte Haupthalle</title>
             <path
                 fill="#929292"
-                fillOpacity={0.5}
+                fillOpacity="0.5"
                 fillRule="evenodd"
                 stroke="#929292"
-                strokeWidth={10}
-                d="M 225 1030 L 372 1010.710693 L 336 840 L 225 840 L 225 1030 Z"
+                strokeWidth="10"
+                d="m225 1030 147-19.29L336 840H225z"
             />
             <path
                 fill="#929292"
-                fillOpacity={0.5}
+                fillOpacity="0.5"
                 fillRule="evenodd"
                 stroke="#929292"
-                strokeWidth={10}
-                d="M 325 766 L 325 744 L 377 744 L 313 559 L 267 559 L 197 442 L 103 569 L 147 677 L 239 739 L 227 764 Z"
+                strokeWidth="10"
+                d="M325 766v-22h52l-64-185-66 9-55-115-89 116 44 108 92 62-12 25Z"
             />
             <path
                 fill="none"
                 stroke="#929292"
-                strokeWidth={10}
-                d="M 631 363 L 631 392 L 457 447 L 400 447 L 412 529 L 467 529 L 619 475 L 666 475 L 666 586 L 616 626 L 666 666 L 666 775 L 492 775 L 492 1102"
+                strokeWidth="10"
+                d="M631 363v29l-174 55h-57l12 82h55l152-54h47v111l-50 40 50 40v109H487"
             />
             <path
                 fill="none"
                 stroke="#929292"
-                strokeWidth={10}
-                d="M 23 25 L 24 145 L 31 166 L 31 258 L 54 261 L 54 281 L 31 284 L 31 333 L 5 427 L 5 775 L 5 1029 L 131 1029 L 131 1097 L 350 1097"
-            />
-            <path
-                fill="none"
-                stroke="#929292"
-                strokeWidth={10}
-                d="M 112 25 L 114 156 L 155 156 L 155 125 L 308 125 L 323 158 L 367 158 L 395 126 L 395 27 L 498 27 L 498 128 L 533 153 L 429 230 L 388 322 L 400 363 L 448 363 L 507 279 L 632 279 L 632 304"
-            />
-            <path
-                fill="none"
-                stroke="#929292"
-                strokeWidth={10}
-                strokeOpacity={0.5}
                 strokeDasharray="20 10"
-                strokeDashoffset={0}
-                d="M 401.000305 363 L 399.99942 444"
+                strokeDashoffset="0"
+                strokeOpacity="0.5"
+                strokeWidth="10"
+                d="M492 780v322"
             />
             <path
                 fill="none"
                 stroke="#929292"
-                strokeWidth={10}
-                strokeOpacity={0.5}
+                strokeWidth="10"
+                d="m23 25 1 120 7 21v92l23 3v20l-23 3v49L5 427v602h126v68h219"
+            />
+            <path
+                fill="none"
+                stroke="#929292"
+                strokeWidth="10"
+                d="m112 25 2 131h41v-31h153l15 33h44l28-32V27h103v101l35 25-104 77-41 92 12 41h48l59-84h125v25"
+            />
+            <path
+                fill="none"
+                stroke="#929292"
                 strokeDasharray="20 10"
-                strokeDashoffset={0}
-                d="M 197 438.999573 L 4.999919 433.000793"
+                strokeDashoffset="0"
+                strokeOpacity="0.5"
+                strokeWidth="10"
+                d="m401 363-1 81"
             />
             <path
                 fill="none"
                 stroke="#929292"
-                strokeWidth={10}
-                strokeOpacity={0.5}
                 strokeDasharray="20 10"
-                strokeDashoffset={0}
-                d="M 107.000038 567.999817 L 3.000009 561.999817"
+                strokeDashoffset="0"
+                strokeOpacity="0.5"
+                strokeWidth="10"
+                d="M192 449 5 433"
             />
             <path
                 fill="none"
                 stroke="#929292"
-                strokeWidth={10}
-                strokeOpacity={0.5}
                 strokeDasharray="20 10"
-                strokeDashoffset={0}
-                d="M 224.999924 997.999817 L 131.999863 1029.999634"
+                strokeDashoffset="0"
+                strokeOpacity="0.5"
+                strokeWidth="10"
+                d="m191 448 121 109"
             />
             <path
                 fill="none"
                 stroke="#929292"
-                strokeWidth={10}
-                strokeOpacity={0.5}
                 strokeDasharray="20 10"
-                strokeDashoffset={0}
-                d="M 227.000137 1028.000366 L 133.999863 1058.999634"
+                strokeDashoffset="0"
+                strokeOpacity="0.5"
+                strokeWidth="10"
+                d="M107 568 3 562"
             />
             <path
                 fill="none"
                 stroke="#929292"
-                strokeWidth={10}
-                strokeOpacity={0.5}
                 strokeDasharray="20 10"
-                strokeDashoffset={0}
-                d="M 225 765 L 225 840"
+                strokeDashoffset="0"
+                strokeOpacity="0.5"
+                strokeWidth="10"
+                d="m225 998-93 32"
             />
             <path
                 fill="none"
                 stroke="#929292"
-                strokeWidth={10}
-                strokeOpacity={0.5}
                 strokeDasharray="20 10"
-                strokeDashoffset={0}
-                d="M 322.999542 762.000122 L 335.999969 841"
+                strokeDashoffset="0"
+                strokeOpacity="0.5"
+                strokeWidth="10"
+                d="m227 1028-93 31"
             />
             <path
                 fill="none"
                 stroke="#929292"
-                strokeWidth={10}
-                d="M 651.256165 333.203552 L 637.492981 339.407043 L 637.492981 333.45166 L 611.908875 333.45166 L 611.908875 332.955383 L 637.492981 332.955383 L 637.492981 327 Z"
+                strokeDasharray="20 10"
+                strokeDashoffset="0"
+                strokeOpacity="0.5"
+                strokeWidth="10"
+                d="M225 765v75"
             />
             <path
                 fill="none"
                 stroke="#929292"
-                strokeWidth={10}
-                d="M 421.203522 1079.743774 L 427.407043 1093.50708 L 421.45166 1093.50708 L 421.45166 1119.091064 L 420.955383 1119.091064 L 420.955383 1093.50708 L 415 1093.50708 Z"
+                strokeDasharray="20 10"
+                strokeDashoffset="0"
+                strokeOpacity="0.5"
+                strokeWidth="10"
+                d="m323 762 13 79"
             />
             <path
                 fill="none"
                 stroke="#929292"
-                strokeWidth={10}
-                d="M 69.796478 45.256226 L 63.592957 31.49292 L 69.54834 31.49292 L 69.54834 5.908936 L 70.044617 5.908936 L 70.044617 31.49292 L 76 31.49292 Z"
+                strokeWidth="10"
+                d="m651.256 333.204-13.763 6.203v-5.955h-25.584v-.497h25.584V327Z"
+            />
+            <path
+                fill="none"
+                stroke="#929292"
+                strokeWidth="10"
+                d="m421.204 1079.744 6.203 13.763h-5.955v25.584h-.497v-25.584H415Z"
+            />
+            <path
+                fill="none"
+                stroke="#929292"
+                strokeWidth="10"
+                d="m69.796 45.256-6.203-13.763h5.955V5.909h.497v25.584H76Z"
             />
             <path
                 fill="#74fbea"
-                fillOpacity={0.5}
+                fillOpacity="0.5"
                 fillRule="evenodd"
                 stroke="#74fbea"
-                strokeWidth={10}
-                d="M 129.69873 312.30127 L 216.30127 362.30127 L 266.30127 275.69873 L 179.69873 225.69873 L 129.69873 312.30127 Z"
+                strokeWidth="10"
+                d="m129.699 312.301 86.602 50 50-86.602-86.602-50z"
             />
 
             {props.children}
